284c272c5ac9f0f33051d7dd49b7907cc07e1dc2
Selvan Palanisamy <selvan.palanisamy@beamteq.com>
2024-02-03 20:16:00 -0500

    first refactor

--- scripts/git/merge-from-gh-to-localdev.sh
+++ scripts/git/merge-from-gh-to-localdev.sh
@@ -1,48 +1,48 @@
 #!/bin/sh
 
 echo "-- merge to local started"
-HOMEFOLDER=/c/Users/selva
-PROJBASEFOLDER=$HOMEFOLDER/Documents/code/etogrow
-cd $PROJBASEFOLDER
+HOME_FOLDER=/c/Users/selva
+PROJECT_BASE_FOLDER=$HOME_FOLDER/Documents/code/etogrow
+cd $PROJECT_BASE_FOLDER
 GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519_bb-bqin-sp-beamteq-com-np"
 for dir in */; do
     if [ -d "$dir" ]; then
-		GITREPO="$dir"
-		EB1="- --"
-		EB2="- ----"
-		EB3="- ------"
-		ccd $PROJBASEFOLDER/$GITREPO
-		if [[ $GITREPO == *-lib* ]]; then
-			GITBRANCH=main
-		else
-			GITBRANCH=develop
-		fi
-		UPSTREAM=${1:-'@{u}'}
-			
-		LOCAL=$(git rev-parse @)
-		BBREMOTE=$(git rev-parse "$UPSTREAM")
-		BASE=$(git merge-base @ "$UPSTREAM")
-		
-		#echo $EB3$LOCAL
-		#echo $EB3$BBREMOTE
-		#echo $EB3$BASE
+        GITREPO="$dir"
+        EB1="- --"
+        EB2="- ----"
+        EB3="- ------"
+        ccd $PROJECT_BASE_FOLDER/$GITREPO
+        if [[ $GITREPO == *-lib* ]]; then
+            GITBRANCH=main
+        else
+            GITBRANCH=develop
+        fi
+        UPSTREAM=${1:-'@{u}'}
+
+        LOCAL=$(git rev-parse @)
+        BBREMOTE=$(git rev-parse "$UPSTREAM")
+        BASE=$(git merge-base @ "$UPSTREAM")
+
+        #echo $EB3$LOCAL
+        #echo $EB3$BBREMOTE
+        #echo $EB3$BASE
 
 
-		echo $EB2$GITREPO"("$GITBRANCH")"
-		if [ $LOCAL = $BBREMOTE ]; then
-			echo $EB3" - Local already has the latest"
-		elif [ $LOCAL = $BASE ]; then
-			#git -c core.quotepath=false -c log.showSignature=false fetch https://username:password@bitbucket.org/beamteq/$GITREPO.git $GITBRANCH --recurse-submodules=no --progress --prune
-			git -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune
-			echo $EB3"Fetched"
-			git -c core.quotepath=false -c log.showSignature=false merge origin/$GITBRANCH --no-stat -v
-			echo $EB3"Merged"
-		elif [ $BBREMOTE = $BASE ]; then
-			echo $EB3"Need to push"
-		else
-			echo $EB3"Diverged"
-		fi
-	fi
-	cd $PROJBASEFOLDER
+        echo $EB2$GITREPO"("$GITBRANCH")"
+        if [ $LOCAL = $BBREMOTE ]; then
+            echo $EB3" - Local already has the latest"
+        elif [ $LOCAL = $BASE ]; then
+            #git -c core.quotepath=false -c log.showSignature=false fetch https://username:password@bitbucket.org/beamteq/$GITREPO.git $GITBRANCH --recurse-submodules=no --progress --prune
+            git -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune
+            echo $EB3"Fetched"
+            git -c core.quotepath=false -c log.showSignature=false merge origin/$GITBRANCH --no-stat -v
+            echo $EB3"Merged"
+        elif [ $BBREMOTE = $BASE ]; then
+            echo $EB3"Need to push"
+        else
+            echo $EB3"Diverged"
+        fi
+    fi
+    cd $PROJECT_BASE_FOLDER
 done
 echo "-- merge to local ended"
--- scripts/git/push-from-local-to-ghspd.sh
+++ scripts/git/push-from-local-to-ghspd.sh
@@ -1,47 +1,47 @@
 #!/bin/sh
 
 echo "-- push started"
-HOMEFOLDER=/c/Users/selva
-PROJBASEFOLDER=$HOMEFOLDER/Documents/code/sync-bb-sp2-to-gh-spd
-cd $PROJBASEFOLDER
+HOME_FOLDER=/c/Users/selva
+PROJECT_BASE_FOLDER=$HOME_FOLDER/Documents/code/sync-bb-sp2-to-gh-spd
+cd $PROJECT_BASE_FOLDER
 GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519_gh-spd-tpselvan-gmail-com-np"
 for dir in */; do
     if [ -d "$dir" ]; then
-		GITREPO="$dir"
-		EB1="- --"
-		EB2="- ----"
-		EB3="- ------"
-		cd $PROJBASEFOLDER/$GITREPO
-		if [[ $GITREPO == *-lib* ]]; then
-			GITBRANCH=main
-		else
-			GITBRANCH=develop
-		fi
-		UPSTREAM=${1:-'@{u}'}
-			
-		LOCAL=$(git rev-parse @)
-		BBREMOTE=$(git rev-parse "$UPSTREAM")
-		BASE=$(git merge-base @ "$UPSTREAM")
-		
-		#echo $EB3$LOCAL
-		#echo $EB3$BASE
-		
-		GHREMOTE=$(git rev-parse github-spd/$GITBRANCH)
-		#echo $EB3$GHREMOTE
-		echo $EB2$GITREPO"("$GITBRANCH")"
-		if [ $GHREMOTE != $BASE ]; then
-			echo $EB3"Need to push"
-			if [ $GITREPO == "survey-be-lib" ]; then
-				#git lfs push --all github-spd $GITBRANCH
-				echo $EB3"Ignored"
-			else
-				git -c core.quotepath=false -c log.showSignature=false push --progress --porcelain github-spd refs/heads/$GITBRANCH:$GITBRANCH
-				echo $EB3"Pushed"
-			fi
-		else
-			echo $EB3"GH Remote already has the latest"
-		fi
-	fi
-	cd $PROJBASEFOLDER
+        GITREPO="$dir"
+        EB1="- --"
+        EB2="- ----"
+        EB3="- ------"
+        cd $PROJECT_BASE_FOLDER/$GITREPO
+        if [[ $GITREPO == *-lib* ]]; then
+            GITBRANCH=main
+        else
+            GITBRANCH=develop
+        fi
+        UPSTREAM=${1:-'@{u}'}
+
+        LOCAL=$(git rev-parse @)
+        BBREMOTE=$(git rev-parse "$UPSTREAM")
+        BASE=$(git merge-base @ "$UPSTREAM")
+
+        #echo $EB3$LOCAL
+        #echo $EB3$BASE
+
+        GHREMOTE=$(git rev-parse github-spd/$GITBRANCH)
+        #echo $EB3$GHREMOTE
+        echo $EB2$GITREPO"("$GITBRANCH")"
+        if [ $GHREMOTE != $BASE ]; then
+            echo $EB3"Need to push"
+            if [ $GITREPO == "survey-be-lib" ]; then
+                #git lfs push --all github-spd $GITBRANCH
+                echo $EB3"Ignored"
+            else
+                git -c core.quotepath=false -c log.showSignature=false push --progress --porcelain github-spd refs/heads/$GITBRANCH:$GITBRANCH
+                echo $EB3"Pushed"
+            fi
+        else
+            echo $EB3"GH Remote already has the latest"
+        fi
+    fi
+    cd $PROJECT_BASE_FOLDER
 done
 echo "-- push ended"
