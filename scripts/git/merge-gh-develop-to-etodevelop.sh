7fa5d4ad94d60763f808b9ac0df5900c1c7b9e57
Selvan <selvan@beamteq.com>
2024-02-17 18:14:28 -0500

    improve script syntax

--- scripts/git/merge-gh-develop-to-etodevelop.sh
+++ scripts/git/merge-gh-develop-to-etodevelop.sh
@@ -3,20 +3,16 @@
 echo "-- merge to local started"
 HOME_FOLDER=/c/Users/selva
 PROJECT_BASE_FOLDER=$HOME_FOLDER/Documents/code/etogrow
-cd $PROJECT_BASE_FOLDER
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519_gh-spd-tpselvan-gmail-com-np"
+cd $PROJECT_BASE_FOLDER || exit
+
 for dir in */; do
-    if ([ -d "$dir" ] && [[ "$dir" != *-lib* ]]); then
-        # if [ "$dir" == "common-service-be/" ]; then
-          # continue
-        # fi
+    if [ -d "$dir" ] && [ "$dir" != "*-lib*" ]; then
         GIT_REPO="$dir"
-        echo $EB2$GIT_REPO
-        EB1="- --"
+        echo "$EB2""$GIT_REPO"
         EB2="- ----"
         EB3="- ------"
-        cd $PROJECT_BASE_FOLDER/$GIT_REPO
-        if [[ $GIT_REPO == *-lib* ]]; then
+        cd $PROJECT_BASE_FOLDER/"$GIT_REPO" || exit
+        if [ "$GIT_REPO" == "*-lib*" ]; then
             GIT_BRANCH=eto-develop
         else
             GIT_BRANCH=eto-develop
@@ -24,37 +20,33 @@ for dir in */; do
         UPSTREAM=${1:-'@{u}'}
 
         LOCAL=$(git rev-parse @)
-        BBREMOTE=$(git rev-parse "$UPSTREAM")
+        #BBREMOTE=$(git rev-parse "$UPSTREAM")
         BASE=$(git merge-base @ "$UPSTREAM")
 
-        #echo $EB3$LOCAL
-        #echo $EB3$BBREMOTE
-        #echo $EB3$BASE
-
         git -c core.quotepath=false -c log.showSignature=false fetch origin --recurse-submodules=no --progress --prune
-        echo $EB3"Fetched"
+        echo "$EB3""Fetched"
         git -c core.quotepath=false -c log.showSignature=false fetch origin develop:develop --recurse-submodules=no --progress --prune
-        echo $EB3"Merged develop into develop"
-        echo $EB3"Merged eto-develop into eto-develop"
+        echo "$EB3""Merged develop into develop"
+        echo "$EB3""Merged eto-develop into eto-develop"
         git -c core.quotepath=false -c log.showSignature=false merge develop --no-edit
-        echo $EB3"Merged develop into eto-develop"
+        echo "$EB3""Merged develop into eto-develop"
 
         LOCAL=$(git rev-parse @)
         BASE=$(git merge-base @ "$UPSTREAM")
-        GHREMOTE=$(git rev-parse origin/eto-develop)
+        GIT_HUB_REMOTE=$(git rev-parse origin/eto-develop)
 
-        echo $EB3$BASE
-        echo $EB3$GHREMOTE
-        echo $EB3$LOCAL
+        echo "$EB3""$BASE"
+        echo "$EB3""$GIT_HUB_REMOTE"
+        echo "$EB3""$LOCAL"
 
-        if ([ $GHREMOTE != $BASE ] || [ $GHREMOTE != $LOCAL ]); then
-            echo $EB3"Need to push"
+        if [ "$GIT_HUB_REMOTE" != "$BASE" ] || [ "$GIT_HUB_REMOTE" != "$LOCAL" ]; then
+            echo "$EB3""Need to push"
             git -c core.quotepath=false -c log.showSignature=false push --progress --porcelain origin refs/heads/$GIT_BRANCH:$GIT_BRANCH
-            echo $EB3"Pushed"
+            echo "$EB3""Pushed"
         else
-            echo $EB3"GH Remote already has the latest"
+            echo "$EB3""GH Remote already has the latest"
         fi
     fi
-    cd $PROJECT_BASE_FOLDER
+    cd $PROJECT_BASE_FOLDER || exit
 done
 echo "-- merge to local ended"
