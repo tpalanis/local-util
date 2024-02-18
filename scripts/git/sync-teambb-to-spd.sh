a0eab885398031e9f8339419078ae6504d91750c
Selvan <selvan@beamteq.com>
2024-02-17 23:53:23 -0500

    change script path to relative

--- scripts/git/sync-teambb-to-spd.sh
+++ scripts/git/sync-teambb-to-spd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo "sync started"
-/c/Users/selva/Documents/ndev/local-util/scripts/git/merge-from-teambb-to-local.sh
-/c/Users/selva/Documents/ndev/local-util/scripts/git/push-from-local-to-ghspd.sh
+./merge-from-teambb-to-local.sh
+./push-from-local-to-ghspd.sh
 echo "sync ended"
